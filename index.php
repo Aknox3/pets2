ea67bd8d9362493cc77c82c55b301a0c670b5896
Ashton Knox <aknox3@mail.greenriver.edu>
2018-01-18 16:48:39 -0500

    added a default view

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
+
     require_once "vendor/autoload.php";
 
     $f3 = Base::instance();
 
-    $f3->route('GET /', function() {
-        echo '<h1>Hello, world!</h1>';
-    });
+$f3->route('GET /', function() {
+    //echo '<h1>This is the Toe Rings page</h1>';
+    $template = new Template();
+    echo $template->render('views/home.html');
+});
+
     $f3->run();
 ?>
