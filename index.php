c492dc27c92b831c4a3ee2cc6e3b78647d18fad6
JakeLandowski <jakelandowski@gmail.com>
2018-01-18 14:05:59 -0800

    fixed 404 error check

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $f3->route('GET /pets/show/@type', function($f3, $params)
                     'cat' => 'http://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg',
                     'gazelle' => 'https://www.worldatlas.com/r/w728-h425-c728x425/upload/aa/ec/e9/shutterstock-368215916.jpg'
                 ];
-    if(!in_array($params['type'], $pictures))
+    if(!array_key_exists($params['type'], $pictures))
     {
         $f3->error(404);
     }
