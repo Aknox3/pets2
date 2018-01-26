150a30884e18251171418addfb8922a401713fd1
Mathias Taylor <mtaylor53@mail.greenriver.edu>
2018-01-26 17:32:05 -0500

    set the colors array

--- index.php
+++ index.php
@@ -12,8 +12,13 @@ $f3 = Base::instance();
 $f3->set('DEBUG', 3);
 $f3->set('colors', array('green','blue','pink'));
 
-$f3->route('GET /', function() 
+$f3->route('GET /', function($f3)
 {
+    if(isset($_POST['submit'])){
+        include 'model/validate.php';
+        $color = $_POST['color'];
+        $f3->set('color',$color);
+    }
 
     $template = new Template();
     echo $template->render('views/home.html');
