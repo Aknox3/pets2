2849ef36fb375347608456ea242f4f130466ecd9
Ashton Knox <aknox3@mail.greenriver.edu>
2018-01-26 17:19:14 -0500

    VALIDATE COLORS

---
+++ model/validate.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ashton Knox
+ * Date: 1/26/2018
+ * Time: 2:17 PM
+ */
+
+/*
+ * @param String color
+ * @return boolean
+ */
+function validColor($color)
+{
+    global $f3;
+    return in_array($color, $f3->get('colors'));
+}
